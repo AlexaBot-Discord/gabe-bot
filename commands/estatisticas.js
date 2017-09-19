d741e0b20b784f2225ebf03ef4880e5034a5eb9f
Paiva <kvrazx@gmail.com>
2017-09-19 07:30:09 +0100

    update $estatisticas

--- commands/estatisticas.js
+++ commands/estatisticas.js
@@ -8,8 +8,8 @@ String.prototype.toHHMMSS = function () {
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+'h '+minutes+'m '+seconds+'s';
-        days > 1 ? time = days+" days " : time = time
+    var time    = hours+' horas, '+minutes+' minutos e '+seconds+' segundos';
+        days > 1 ? time = days+" dias " : time = time
     return time;
 };
     var os = require('os-utils');
