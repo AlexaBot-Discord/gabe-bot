01ff25afbac255a0e7d0473568e7d9e02fe97726
Paiva <kvrazx@gmail.com>
2017-09-26 22:42:03 +0100

    MESSAGE IS NOT DEFINED

--- bot.js
+++ bot.js
@@ -15,22 +15,22 @@ client.on("ready", () => {
 // Commands
 client.on("message", msg => {
   // Start Points
-  sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+  sql.get(`SELECT * FROM scores WHERE userId ="${msg.author.id}"`).then(row => {
     if (!row) {
-      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [msg.author.id, 1, 0]);
     } else {
       let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
       if (curLevel > row.level) {
         row.level = curLevel;
-        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
-        message.reply(`Você subiu para o nível **${curLevel}**! Tu tá de parabéns, meliante!`);
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${msg.author.id}`);
+        msg.reply(`Você subiu para o nível **${curLevel}**! Tu tá de parabéns, meliante!`);
       }
-      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${msg.author.id}`);
     }
   }).catch(() => {
     console.error;
     sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
-      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [msg.author.id, 1, 0]);
     });
   });
   // End Points
--- commands/nivel.js
+++ commands/nivel.js
@@ -1,7 +1,7 @@
 exports.run = (client, msg, args, sql) => {
   msg.delete();
   sql.get(`SELECT * FROM scores WHERE userId ="${msg.author.id}"`).then(row => {
-    if (!row) return message.reply("Teu nível atual é **0**, parça.");
-    message.reply(`Teu nível atual é **${row.level}**, parça.`);
+    if (!row) return msg.reply("Teu nível atual é **0**, parça.");
+    msg.reply(`Teu nível atual é **${row.level}**, parça.`);
   });
 }
