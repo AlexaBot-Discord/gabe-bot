f0e61518547580731102b506e5c6356e89714793
PillGP <pill@apydle.com>
2017-10-01 14:04:30 +0100

    Bugfiqs

--- commands/estatisticas.js
+++ commands/estatisticas.js
@@ -1,14 +1,14 @@
 exports.run = (client, msg, args) => {
 String.prototype.toHHMMSS = function () {
-    const sec_num = parseInt(this, 10); 
-    const hours   = Math.floor(sec_num / 3600);
-    const days   = Math.floor(hours / 24);
-    const minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    const seconds = sec_num - (hours * 3600) - (minutes * 60);
+    var sec_num = parseInt(this, 10); 
+    var hours   = Math.floor(sec_num / 3600);
+    var days   = Math.floor(hours / 24);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    const time    = hours+'h '+minutes+'m '+seconds+'s';
+    var time    = hours+'h '+minutes+'m '+seconds+'s';
         days > 1 ? time = days+" dias " : time = time
     return time;
 };
@@ -16,7 +16,7 @@ String.prototype.toHHMMSS = function () {
     const moment = require('moment');
     const sistema = os.sysUptime();
     msg.channel.startTyping();
-    msg.channel.send({embed: {
+    msg.channel.send("...").then(m => m.edit({embed: {
         author: {
             name: "Estatísticas do " + client.user.username,
             icon_url: client.user.avatarURL
@@ -37,7 +37,7 @@ String.prototype.toHHMMSS = function () {
                 inline: true
             },{
                 name: ":signal_strength: | **Ping**",
-                value: `${Math.round(client.ping)}ms`,
+                value: `Ping: ${m.createdTimestamp - msg.createdTimestamp}ms\nHeartbeat: ${Math.round(client.ping)}ms`,
                 inline: true
             },{
                 name: ":electric_plug: | **Uptime do Bot**",
@@ -53,6 +53,6 @@ String.prototype.toHHMMSS = function () {
             icon_url: msg.author.avatarURL,
             text: `${msg.author.tag}`
         }
-    }});
+    }}));
     msg.channel.stopTyping();
 }
